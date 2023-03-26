f6bbe20c30b89b0a7e3579a89b809f17658ba199
AndyFooGuoZhen <ndfucoding@gmail.com>
2023-03-25 23:12:33 -0500

    NextJs first tutorial

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+     
+        <p>Hi! My name is Andy Foo. I'm a CS student in Iowa State University.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
