27b81ca393c7f5150c3f3566c312198fa7e1ecfb
AndyFooGuoZhen <ndfucoding@gmail.com>
2023-03-28 22:03:29 -0500

    Api endpoints

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,11 +1,11 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import Image from 'next/image';
-import Link from 'next/link';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Image from "next/image";
+import Link from "next/link";
 // import Date from '../components/date';
 
-import { getSortedPostsData } from '../lib/posts';
+import { getSortedPostsData } from "../lib/posts";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -23,10 +23,11 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-     
-        <p>Hi! My name is Andy Foo. I'm a CS student in Iowa State University.</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          Hi! My name is Andy Foo. I'm a CS student in Iowa State University.
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
@@ -35,16 +36,15 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>{title}</Link>
-            <br />
-            {/* <small className={utilStyles.lightText}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              {/* <small className={utilStyles.lightText}>
               <Date dateString={date} />
             </small> */}
-          </li>
+            </li>
           ))}
         </ul>
       </section>
-
     </Layout>
   );
 }
